58ded56c51dec93ca670cfd1de73df43e8894dcb
HakanKara75 <hakanbatirhan@gmail.com>
2023-09-15 12:40:47 +0300

    v

--- Cypress/e2e/1-getting-started/faker.cy.js
+++ Cypress/e2e/1-getting-started/faker.cy.js
@@ -12,9 +12,25 @@ kullanilan dosyada import yapilir=> import { faker } from '@faker-js/faker' ile
 
     it('Faker Test', ()=>{
     cy.visit('https://www.pazarama.com.tr')
-    cy.get('.dn-slide-body > .dn-slide-buttons > .dn-slide-accept-btn').click()
     cy.get('.pl-7 > .btn').click()
-    //let firstname=faker.
+
+    cy.get(':nth-child(2) > .card__passive-page > span').click()
+
+    let firstname=faker.person.firstName('male')
+    cy.get('#FirstName').type(firstname)
+    let surname=faker.person.lastName()
+    cy.get('#LastName').type(surname)
+
+    let email = faker.internet.email()
+    cy.get('#SignupUserName').type(email)
+
+    let password = faker.internet.password()
+    cy.get('#registerPassword').type(password)
+
+    let cellphone= faker.phone.number()
+    cy.get('#Gsm').type(cellphone)
+   
+   
     })
     
     
